37e7c02cc00b2df074490c435e7dc67a106f966a
AIRIEL D. SALVATORE <AIRIEL.D.SALVATORE@GMAIL.COM>
2016-10-29 16:05:13 -0700

    Pull filter links into a Footer component

---
+++ src/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import FilterLink from './filterLink'
+
+const Footer = ({ visibilityFilter, onFilterClick }) => (
+  <p>
+    Show:
+    {' '}
+    <FilterLink filter="SHOW_ALL">
+      All
+    </FilterLink>
+    {', '}
+    <FilterLink filter="ACTIVE">
+      Active
+    </FilterLink>
+    {', '}
+    <FilterLink filter="COMPLETED">
+      Completed
+    </FilterLink>
+  </p>
+)
+
+export default Footer
