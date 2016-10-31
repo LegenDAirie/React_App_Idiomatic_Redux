860629e08a07b623b312b38704b6c7db01e3053a
AIRIEL D. SALVATORE <AIRIEL.D.SALVATORE@GMAIL.COM>
2016-10-31 12:59:31 -0700

    Use provider to access store via context

--- src/index.js
+++ src/index.js
@@ -4,26 +4,26 @@ import App from './App'
 import { createStore } from 'redux'
 import todoAppReducer from './stateManagement/reducers/todoApp'
 
-// const Provider = React.createClass({
-//
-//   getChildContext () {
-//     return {
-//       store: this.props.store
-//     }
-//   },
-//
-//   render () {
-//     return this.props.children
-//   }
-// })
-//
-// Provider.childContextTypes = {
-//   store: React.PropTypes.object
-// }
+const Provider = React.createClass({
+
+  getChildContext () {
+    return {
+      store: this.props.store
+    }
+  },
+
+  render () {
+    return this.props.children
+  }
+})
+
+Provider.childContextTypes = {
+  store: React.PropTypes.object
+}
 
 ReactDOM.render(
-  // <Provider store={ createStore(todoAppReducer) } >
-    <App store={ createStore(todoAppReducer) } />
-  // </Provider>,
+  <Provider store={ createStore(todoAppReducer) } >
+    <App />
+  </Provider>,
   document.getElementById('root')
 )
