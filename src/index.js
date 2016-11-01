bf27c71c82ea97a27881264bd3cc59c3ff110094
AIRIEL D. SALVATORE <AIRIEL.D.SALVATORE@GMAIL.COM>
2016-11-01 14:19:55 -0700

    Extract store creation logic into own file

--- src/index.js
+++ src/index.js
@@ -1,22 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import { createStore } from 'redux'
-import todoAppReducer from './stateManagement/reducers/todoApp'
 import { Provider } from 'react-redux'
-import { loadState, saveState } from './localStorage'
-import throttle from 'lodash/throttle'
+import configureStore from './configureStore'
 
-const persistedState = loadState()
-
-const store = createStore(todoAppReducer, persistedState)
-
-store.subscribe( throttle( () => {
-  saveState(
-    { todos: store.getState().todos
-    }
-  )
-}, 1000))
+const store = configureStore()
 
 ReactDOM.render(
   <Provider store={ store }>
--- src/store.js
+++
@@ -1,6 +0,0 @@
-import { createStore } from 'redux'
-import todoAppReducer from './stateManagement/reducers/todoApp'
-
-// const store = createStore(todoAppReducer)
-//
-// export default store
