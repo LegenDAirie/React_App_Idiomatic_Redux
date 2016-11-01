e6495cb47b653fd12668b8daa73c32550ebf87d5
AIRIEL D. SALVATORE <AIRIEL.D.SALVATORE@GMAIL.COM>
2016-11-01 14:30:45 -0700

    Wrap provider

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
-import { Provider } from 'react-redux'
+import Root from './Root'
+
 import configureStore from './configureStore'
 
 const store = configureStore()
 
 ReactDOM.render(
-  <Provider store={ store }>
-    <App />
-  </Provider>,
+  <Root store={ store } />,
   document.getElementById('root')
 )
 
