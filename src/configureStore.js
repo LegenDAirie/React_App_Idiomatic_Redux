f20745c513af5a100ab7baec1819309ec7bd619a
AIRIEL D. SALVATORE <AIRIEL.D.SALVATORE@GMAIL.COM>
2016-11-02 15:09:21 -0700

    Log state by decorating dispatch

--- src/configureStore.js
+++ src/configureStore.js
@@ -9,6 +9,30 @@ const configureStore = () => {
 
   const store = createStore(todoAppReducer, persistedState)
 
+
+  const logDispatch = (store) => {
+    const rawDispatch = store.dispatch
+
+    if (!console.group) {
+      return rawDispatch
+    }
+
+    return (action) => {
+      console.group(action.type)
+      console.log('%c Old State: ', "color: grey", store.getState())
+      console.log('%c Action: ', 'color: blue', action)
+      const returnValue = rawDispatch(action)
+      console.log('%c New State: ', 'color: green', store.getState())
+      console.groupEnd(action.type)
+
+      return returnValue
+    }
+  }
+
+  if (process.env.NODE_ENV !== 'production') {
+    store.dispatch = logDispatch(store)
+  }
+
   store.subscribe( throttle( () => {
     saveState(
       { todos: store.getState().todos
